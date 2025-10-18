a3691fc1da102f05a8e066ede4dfa7e90f897f96
Aleksei Seren <aseren@brave.com>
2025-10-18 13:00:15 -0500

    Add BRAVE_ADS content setting policy
    This change allows to set BRAVE_ADS
    content setting via managed preference
    basing on policy.

--- browser/policy/brave_simple_policy_map.h
+++ browser/policy/brave_simple_policy_map.h
@@ -115,6 +115,8 @@ inline constexpr PolicyToPreferenceMapEntry kBraveSimplePolicyMap[] = {
      base::Value::Type::BOOLEAN},
     {policy::key::kDefaultBraveFingerprintingV2Setting,
      kManagedDefaultBraveFingerprintingV2, base::Value::Type::INTEGER},
+    {policy::key::kDefaultBraveAdsSetting, kManagedDefaultBraveAds,
+     base::Value::Type::INTEGER},
 };
 
 }  // namespace policy
