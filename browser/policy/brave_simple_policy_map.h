d1476261182d01999993b669b9beb5dfb295365f
Aleksei Seren <aseren@brave.com>
2025-10-18 13:01:29 -0500

    Add BRAVE_REFERRERS content setting policy
    This change allows to set BRAVE_REFERRERS
    content setting via managed preference
    basing on policy.

--- browser/policy/brave_simple_policy_map.h
+++ browser/policy/brave_simple_policy_map.h
@@ -115,6 +115,8 @@ inline constexpr PolicyToPreferenceMapEntry kBraveSimplePolicyMap[] = {
      base::Value::Type::BOOLEAN},
     {policy::key::kDefaultBraveFingerprintingV2Setting,
      kManagedDefaultBraveFingerprintingV2, base::Value::Type::INTEGER},
+    {policy::key::kDefaultBraveReferrersSetting, kManagedDefaultBraveReferrers,
+     base::Value::Type::INTEGER},
 };
 
 }  // namespace policy
